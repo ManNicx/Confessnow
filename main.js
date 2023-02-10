de7b1ffa2e44d4478030ce75ae9c38d62f7ef7d0
ManNicx <113735662+ManNicx@users.noreply.github.com>
2023-02-10 13:11:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  alert("Woi Crush");
+  alert("Gusto kita matagal na");
+  alert("Pwede bang ako nalang?");
+  alert("Ayaw mo talaga?");
+  alert("Ako nalang pwede ba?");
+  alert("Sakin kana kasi");
+  alert("Ayaw mo?");
+  alert("Bakit ayaw mo?");
+  alert("Wag kana sa iba");
+  alert("Pwede ba?");
+  $(".container").show();
+  
+  
+})
